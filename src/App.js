16dee4997fe972b83405c8029506e990fc74c418
Hector Garcia Rosales <ghectormus@gmail.com>
2023-12-21 14:31:05 -0600

    add: component to main

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Testimonio from './componentes/Testimonio';
 
 function App() {
   return (
     <div className="App">
-      
+        <Testimonio/>
     </div>
   );
 }
