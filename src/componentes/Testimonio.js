6d624e59760dc24c19dc882e8def4632e012f3ec
Hector Garcia Rosales <ghectormus@gmail.com>
2023-12-20 18:13:18 -0600

    add: fixed values for component

--- src/componentes/Testimonio.js
+++ src/componentes/Testimonio.js
@@ -7,6 +7,11 @@ function Testimonio() {
       className="imagen-testimonio"
       src={require("../imagenes/testimonio-imagen1.png")}
       alt="Foto 1"/>
+      <div className="contenedor-texto-testimonio">
+        <p className="nombre-testimonio">Emma Bostian en Suecia</p>
+        <p className="cargo-testimonio">Ingeniero de software en Makingdevs</p>
+        <p className="texto-testimonio">Gracias a Makingdevs logré dejar un trabajo horrible y poder tener tiempo para mi</p>
+      </div>
     </div>
   );
 }
