66ae9547ef7cedba2cd6da9185b5499237035a5d
Hector Garcia Rosales <ghectormus@gmail.com>
2023-12-24 00:12:06 -0600

    props: adding properties for prop

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import {Testimonio} from './componentes/Testimonio';
 
@@ -7,8 +6,15 @@ function App() {
     <div className="App">
       <div className="contenedor-principal">
         <h1>Esto es lo que nuestros alumnos en makingdevs opinan</h1>
-        <Testimonio/>
-      </div>    
+        <Testimonio
+          nombre="Shawn Wang"
+          pais="Singapur"
+          imagen=""
+          cargo="Ingeniero de Software"
+          empresa="Amazon"
+          testimonio="Da miedo cambiar de carrera. Solo gané la confianza de que podía programar trabajando a través de los cientos de horas de lecciones gratuitas en freeCodeCamp. Dentro de un año tuve un trabajo de seis cifras como ingeniero de software. freeCodeCamp cambió mi vida."
+        />
+      </div>
     </div>
   );
 }
